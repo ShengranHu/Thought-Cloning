f69cc9f7bddff7b93f08a8584122ac40f60dffeb
ethantqiu <ethantqiu@berkeley.edu>
2023-10-25 05:42:45 +0000

    added setup and testing scripts

---
+++ scripts/test_env.py
@@ -0,0 +1,23 @@
+from babyai.levels.test_levels import Level_TestGoToBlocked
+from babyai.levels.iclr19_levels import Level_BossLevelNoUnlock
+from gym_minigrid import rendering
+from matplotlib import pyplot as plt
+import pdb
+import numpy as np
+
+env = Level_BossLevelNoUnlock()
+env.reset()
+for i in range(10):
+    env.step(1)
+    obs = env.step(2)[0]
+    # print(obs)
+    print(str(env))
+# rendering.
+# obs["image"][0][0][0] = 200
+# plt.imshow(obs["image"])
+# plt.show()
+# pdb.set_trace()
+x = env.render()
+plt.imshow(x)
+print(np.average(x))
+plt.savefig("test.png")
---
+++ setup.sh
@@ -0,0 +1,4 @@
+conda create -n tc python==3.9
+conda activate tc
+pip3 install -r requirements.txt
+pip3 install -e .
